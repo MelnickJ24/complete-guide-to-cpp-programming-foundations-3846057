60d2780de427963c0132c5ef0cf9809a2b1845c2
Jordan Melnick <113388001+MelnickJ24@users.noreply.github.com>
2024-12-21 21:04:28 +0000

    added some writing

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03
+// Exercise 00_03 Made a change to the writing 
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
